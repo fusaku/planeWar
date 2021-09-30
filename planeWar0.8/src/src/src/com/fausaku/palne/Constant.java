6155b5915e3e8da1276d1400e7ef0381dac53ec3
Fusaku <youtiao3627@gmail.com>
2021-09-30 20:52:32 +0900

    更新碰撞检测

---
+++ planeWar0.8/src/src/src/com/fausaku/palne/Constant.java
@@ -0,0 +1,11 @@
+package src.src.com.fausaku.palne;
+
+/**
+ * 存放相关的常量
+ */
+public class Constant {
+    //游戏窗口的宽度
+    public static final int GAME_WIDTH = 500;
+    //游戏窗口的高度
+    public static final int GAME_HEIGHT = 500;
+}
